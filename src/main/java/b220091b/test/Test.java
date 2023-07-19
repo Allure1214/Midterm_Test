3593aecab296619eeedc26ca4c2007154b7a6c9d
Allure_1214 <66854547+Allure1214@users.noreply.github.com>
2023-07-19 17:12:08 +0800

    Update Test.java

--- src/main/java/b220091b/test/Test.java
+++ src/main/java/b220091b/test/Test.java
@@ -1,6 +1,6 @@
 package b220091b.test;
 
-iimport javafx.application.Application;
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
